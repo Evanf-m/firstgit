fb4999ba5f515f89dbf9f97db369083ea6dbe940
Evan Ford <evanfordm@outlook.com>
2024-02-02 14:04:33 -0500

    Initial Commit

---
+++ GitEvanFirstTry/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GitEvanFirstTry
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GitEvanFirstTry/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GitEvanFirstTry
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
